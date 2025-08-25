9a10dc53b60c53a5834d73e343204d5c0f1f0cd3
Ayca Dogan <dogan.aycaa@gmail.com>
2025-08-25 19:13:59 +0300

    Add project files.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class,IEntity, new()
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBrandDal : IBrandDal
+    {
+        public void Add(Brand entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Brand entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Brand entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
